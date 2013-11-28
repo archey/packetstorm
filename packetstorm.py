5a9ae85832c7853bd08178cd7720f8a359f6eb43
Tyler Bennett <tylerb@trix2voip.com>
2013-11-28 12:55:39 -0500

    made a few more changes to code, but still needs alot of work

--- packetstorm.py
+++ packetstorm.py
@@ -5,7 +5,13 @@ import sys
 import datetime
 import argparse
 import tarfile
+import progressbar
 from termcolor import colored, cprint
+
+#TODO needs a prgressbar for each download
+#TODO needs more color to tell the user whats going on
+#TODO needs to loop through years from 1999 to find yearly exploits
+#TODO need to loop through months to download montly archives
 
 def get_yearly_exploits(header):
     try:
@@ -13,21 +19,21 @@ def get_yearly_exploits(header):
        now = datetime.datetime.now()
        #url to get yearly exploits from
        url = r'http://dl.packetstormsecurity.net/' + now.strftime("%y") + '12-exploits/' + now.strftime("%Y") + '-exploits.tgz'
+       print url
        #build the request, with the proper user-agent in the custom header
        r = requests.get(url, headers=header, timeout=30)
-       if r.status_code == '200':
-          tarball = r.content
-          print type(tarball)
-       else:
-         status = r.status_code 
-         print status
+       totalsize=r.headers['content-length']
+       if int(totalsize) <= 500:
+          cprint("Fail Whale", "red")
     except requests.exceptions.RequestException, e:
        cprint("An Error has occured: %s" %str(e), 'red', attrs=["bold"])
 
 def get_monthly_exploits(headers):
+    date = datetime.datetime.now()
     months = [ 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec' ]
     for month in months:
         print month
+        
 
 #def search_exploits(search):
     
